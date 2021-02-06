4d77cb9142be353fa84c1e23667c5c1e1ca988d8
shrutika16 <shrutika16dumbre@gmail.com>
2021-02-06 14:17:17 +0530

    first commit

---
+++ database/migrations/2021_02_05_200150_create_post_media_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_media', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('post_id');
+            $table->enum('media_type',['image', 'video']);
+            $table->string('Media_content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_media');
+    }
+}
